4e741980e3bca76d550a40c9ff854c8e0084fa59
Kalaugus <josemagana90@yahoo.com>
2024-03-05 11:33:43 -0800

    not sure what i did here, but i think i added a  nav bar and deleted a react logo

---
+++ Chew-trucks/src/assets/Nav.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>Home</div>
+      <div>schedule</div>
+      <div>calender</div>
+      <div></div>
+    )
+  }
+}
+
+export default Navbar
